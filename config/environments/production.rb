4c8c2558f014f7b880728d15890c7fdb41ec0812
Jared Ables <jaredables@gmail.com>
2016-07-06 13:11:01 -0400

    removing port from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Default URL options for the mailer
-  config.action_mailer.default_url_options = { host: ENV['HOST'], port: ENV['PORT'] }
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
